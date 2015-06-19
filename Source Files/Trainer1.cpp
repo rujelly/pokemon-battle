6f7a877f7a966114568e4741ef820b099ceff0d5
rujelly <andreweroman@gmail.com>
2015-06-19 15:45:38 -0700

    Fix format issue in Trainer1

--- Source Files/Trainer1.cpp
+++ Source Files/Trainer1.cpp
@@ -5,17 +5,18 @@ using namespace std;
 
 class Trainer1 : public Trainer {
 public:
-	  /* This function reads a csv of Pokemon (including health, speed, and other
-     * attributes) and puts each Pokemon in a Pokeball
-     */
+     /* This function reads a csv of Pokemon (including health, speed, and other
+      * attributes) and puts each Pokemon in a Pokeball
+      */
     void capturePokemon(string filepath) {
         ofstream pokedexFile;
         pokedexFile.open("Pokedex.csv");
 
-            // TODO file parsing and creating linked list of Pokeballs
+        // TODO file parsing and creating linked list of Pokeballs
 
         pokedexFile.close();
     }
+    // TODO these 3 mehods
     void setupBooster();
     Pokemon* getNxtPokemon(int opponentHealth,
                           int opponentSpeed,
