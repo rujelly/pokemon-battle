2ae0b23a767c2608e4cf857fb7c87dda646b5b34
rujelly <andreweroman@gmail.com>
2015-06-21 23:03:46 -0700

    Fix enum syntax

--- Header Files/BoosterType.h
+++ Header Files/BoosterType.h
@@ -1,3 +1,11 @@
-enum BoosterType { X-Speed2 = 2, X-Speed3 = 4, X-Speed6 = 16,
-					X-Attack2 = 1, X-Attack3 = 2, X-Attack6 = 5,
-					X-Defense2 = 1, X-Defense3 = 2, X-Defense7 = 7 };
+typedef enum { 
+	X-Speed2 = 2,
+	X-Speed3 = 4,
+	X-Speed6 = 16,
+	X-Attack2 = 1,
+	X-Attack3 = 2,
+	X-Attack6 = 5,
+	X-Defense2 = 1,
+	X-Defense3 = 2,
+	X-Defense7 = 7
+} BoosterType;
--- Header Files/PokemonType.h
+++ Header Files/PokemonType.h
@@ -1,2 +1,18 @@
-enum PokemonType { NORMAL, FIRE, WATER, ELECTRIC, GRASS, ICE, FIGHTING, POISON,
-                       GROUND, FLYING, PSYCHIC, BUG, ROCK, GHOST, DRAGON };
+typedef enum {
+	NONE = -1,
+	NORMAL = 0,
+	FIRE,
+	WATER,
+	ELECTRIC,
+	GRASS,
+	ICE,
+	FIGHTING,
+	POISON,
+	GROUND,
+	FLYING,
+	PSYCHIC,
+	BUG,
+	ROCK,
+	GHOST,
+	DRAGON
+} PokemonType;
