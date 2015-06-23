548c0c3f0658c143990ff6f4ab06efeca4e01334
rujelly <andreweroman@gmail.com>
2015-06-22 17:57:11 -0700

    Copy Trainer1 code to Trainer2

--- Source Files/Trainer2.cpp
+++ Source Files/Trainer2.cpp
@@ -1,15 +1,45 @@
 #include <iostream>
+#include <fstream>
 #include "Trainer.h"
 using namespace std;
 
 class Trainer1 : public Trainer {
 public:
-    void capturePokemon(string filepath);
+     /* This function reads a csv of Pokemon (including health, speed, and other
+      * attributes) and creates a linked list of Pokeballs
+      */
+    void capturePokemon(string filepath) {
+        string name, move, attribute1, attribute2;
+        int HP, strength, defense, speed;
+        Pokeball* pokeballList;
+
+        ofstream pokedexFile("../Resources/Pokedex.csv");
+
+        // read Pokemon attributes from csv and create list of Pokeballs
+        if (file.is_open()) {
+            while(getline(file, name, ',')) {
+              getline(file, stoi(HP, nullptr, 10), ',');
+              getline(file, stoi(strength, nullptr, 10), ',');
+              getline(file, stoi(defense, nullptr, 10), ',');
+              getline(file, stoi(speed, nullptr, 10), ',');
+              getline(file, move, ',');
+              getline(file, attribute1, ',');
+              getline(file, attribute2, ',');
+              Pokemon pokemon(name, attribute1, attribute2, 
+                                  HP, speed, defense, strength, move);
+              pokeballList->content = pokemon;
+              pokeballList = pokeballList->next;
+            }
+        }
+
+        pokedexFile.close();
+    }
+    // TODO these 3 mehods
     void setupBooster();
     Pokemon* getNxtPokemon(int opponentHealth,
-         				   int opponentSpeed,
-                           int opponentStrength,
-                           PokemonType attribute1,
-                           PokemonType attribute2);
+                          int opponentSpeed,
+                          int opponentStrength,
+                          PokemonType attribute1,
+                          PokemonType attribute2);
     Pokemon* selectFirstPokemon();
 };    
